681f093947f9e5429386692285b92cc0cf38f72b
EdwardMelendezM <192666@unsaac.edu.pe>
2023-07-08 08:39:42 -0500

    create new proyect

---
+++ src/screens/HelloWorld.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from 'react-native';
+
+const HelloWorld = () => {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: '#ccc',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Text style={{ fontSize: 45 }} >
+        Hola a todos guys
+      </Text>
+      <Text>Buenos dias a todos gaa</Text>
+    </View>
+   );
+};
+
+export default HelloWorld;
